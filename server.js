79552c2d881a775d28d48cac225297c3a6e2858b
Flo Wil <jmludevid@hotmail.com>
2021-10-31 22:28:10 +1100

    make it better

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const serveStatic = require('serve-static')
 const path = require('path')
 
 const app = express()
-const port = 3000
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 const port = process.env.PORT || 8080
 
 app.listen(port)
+
+console.log('listening to port: ' + port)
